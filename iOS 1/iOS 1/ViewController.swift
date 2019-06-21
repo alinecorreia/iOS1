7a7eba3c0d263edd984e2715263a5bbea7c6e702
Diogo <frogxp@hotmail.com>
2019-06-21 18:04:43 -0300

    Criação do projeto no XCode.

---
+++ iOS 1/iOS 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS 1
+//
+//  Created by Diogo Assis Ferreira on 21/06/19.
+//  Copyright © 2019 Diogo Assis Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
