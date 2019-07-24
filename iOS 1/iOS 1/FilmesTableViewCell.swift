da79898bc9061512d0fa89f8957c49788e83be14
Diogo <frogxp@hotmail.com>
2019-07-24 18:21:19 -0300

    Implementação do Fluxo do App logado

---
+++ iOS 1/iOS 1/FilmesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilmesTableViewCell.swift
+//  iOS 1
+//
+//  Created by Diogo Assis Ferreira on 24/07/19.
+//  Copyright © 2019 Diogo Assis Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class FilmesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
