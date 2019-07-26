79709bfe2ad27bf1ff21fb47edddf410e29c45f5
Diogo <frogxp@hotmail.com>
2019-07-26 15:43:08 -0300

    Fluxo do App logado implementado. Falta corrigir uns detalhes nas constraints

---
+++ iOS 1/iOS 1/RedBorderUITextField.swift
@@ -0,0 +1,41 @@
+//
+//  RedBorderUITextField.swift
+//  iOS 1
+//
+//  Created by Diogo Assis Ferreira on 26/07/19.
+//  Copyright © 2019 Diogo Assis Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class RedBorderUITextField: UITextField {
+    
+    var bottomBorder = UIView()
+    
+    override func awakeFromNib() {
+        
+        // Setup Bottom-Border
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        bottomBorder = UIView.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        bottomBorder.backgroundColor = UIColor.red // Set Border-Color
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(bottomBorder)
+        
+        bottomBorder.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        bottomBorder.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        bottomBorder.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        bottomBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true // Set Border-Strength
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
